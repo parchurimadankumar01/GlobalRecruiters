97dc7c56b59e41b9df54e83371177a3683783487
parchurimadankumar01 <parchurimadankumar01@gmail.com>
2026-02-02 10:33:12 +0530

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 document.getElementById("applyForm").addEventListener("submit", async function (e) {
   e.preventDefault();
 
-  const name = document.getElementById("name").value;
-  const job = document.getElementById("job").value;
-  const phone = document.getElementById("phone").value;
+const name = document.getElementById("name").value;
+const job = document.getElementById("job").value;
+const email = document.getElementById("email").value;
+const phone = document.getElementById("phone").value;
+
 
   console.log("Submitting:", { name, job, phone });
 
@@ -12,13 +14,13 @@ document.getElementById("applyForm").addEventListener("submit", async function (
       name: name,
       job: job,
       phone: phone,
+      email: email,
       createdAt: firebase.firestore.Timestamp.now()
     });
 
     console.log("Saved with ID:", docRef.id);
     alert("Application submitted successfully! We will contact you soon.");
     e.target.reset();
-
   } catch (error) {
     console.error("Firestore error:", error);
     alert("Error saving application. Check console.");
